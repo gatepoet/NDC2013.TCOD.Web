68ec55d6a4c60a6550e4cdab56f2e6a254fe5f96
gatepoet <kristoffer.deinoff@gmail.com>
2013-01-22 15:14:07 +0100

    Added web site

---
+++ src/main/webapp/mvc4webapi/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Itera.Labs.ContinousDelivery.OctopusDemo.mvc4webapi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/main/webapp/mvc4webapi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Itera.Labs.ContinousDelivery.OctopusDemo.mvc4webapi.WebApiApplication" Language="C#" %>
