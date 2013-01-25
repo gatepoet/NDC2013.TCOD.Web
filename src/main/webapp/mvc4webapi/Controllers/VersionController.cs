814bade25112b0e901d0e344dc0de4e572b31a57
gatepoet <kristoffer.deinoff@gmail.com>
2013-01-25 20:24:11 +0100

    Added version api and to web title

---
+++ src/main/webapp/mvc4webapi/Controllers/VersionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Configuration;
+using System.Web.Http;
+
+namespace Itera.Labs.ContinousDelivery.OctopusDemo.mvc4webapi.Controllers
+{
+    public class VersionController : ApiController
+    {
+        public string Get()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
