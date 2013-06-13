5c00731175993804a834e143e074d51a43ce1570
gatepoet <kristoffer.deinoff@gmail.com>
2013-06-13 20:57:21 +0200

    Updated AssemblyInfo

--- src/main/webapp/mvc4webapi/Properties/AssemblyInfo.cs
+++ src/main/webapp/mvc4webapi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("mvc4webapi")]
+[assembly: AssemblyTitle("Itera.NDC2013.TCOD.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("mvc4webapi")]
+[assembly: AssemblyProduct("Itera.NDC2013.TCOD.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
